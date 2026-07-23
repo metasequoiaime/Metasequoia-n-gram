bbf0ff21046a57b0af2d948a85ad84239c65853b
fanlusky <1730976608@qq.com>
2026-07-23 21:50:38 +0800

    Change appdata dir

--- preprocessing/generate_single_hanzi_pinyin_tbl/make_single_pinyin_table.py
+++ preprocessing/generate_single_hanzi_pinyin_tbl/make_single_pinyin_table.py
@@ -141,7 +141,7 @@ with open(
 
 local_app_data_dir = os.environ.get("LOCALAPPDATA")
 db_path = os.path.join(
-    local_app_data_dir, "MetasequoiaImeTsf", "cutted_flyciku_with_jp.db"
+    local_app_data_dir, "metasequoiaime", "cutted_flyciku_with_jp.db"
 )
 
 
